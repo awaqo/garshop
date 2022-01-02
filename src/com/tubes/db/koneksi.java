bc4ed5544768ffc360d93c2b5fb40b7036690c1a
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2022-01-02 09:41:56 +0700

    Menambahkan file Koneksi

---
+++ src/com/tubes/db/koneksi.java
@@ -0,0 +1,24 @@
+package com.tubes.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    Connection conn = null;
+    public static Connection koneksi(){
+        String driver = "com.mysql.jdbc.Driver";
+        String host = "jdbc:mysql://localhost/teknoko";
+        String user = "root";
+        String pass = "";
+        try {
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(host,user,pass);
+            System.out.println("Koneksi Berhasil");
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Koneksi Gagal "+e);
+        }
+        return null;
+    }
+}
